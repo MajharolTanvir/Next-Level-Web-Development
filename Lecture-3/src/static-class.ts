f2445d04a91fe18eb08594d66f5395d3e2913e09
unknown <mdtanvir6070@gmail.com>
2023-05-06 15:43:13 +0600

    Static class

---
+++ Lecture-3/src/static-class.ts
@@ -0,0 +1,17 @@
+//The class make static and then use/access it directly. 
+
+class Counter {
+    static counter: number = 0;
+
+    static increment() {
+    return Counter.counter=Counter.counter + 1
+    }
+
+    static decrement() { 
+        return Counter.counter=Counter.counter - 1
+    }
+}
+
+console.log(Counter.increment()) 
+console.log(Counter.increment()) 
+console.log(Counter.decrement()) 
