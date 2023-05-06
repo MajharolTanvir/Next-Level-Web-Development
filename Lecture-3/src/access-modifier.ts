6a6720f523351753f6faa283e87555152d7cf11c
unknown <mdtanvir6070@gmail.com>
2023-05-06 14:52:47 +0600

    getter and setter

--- Lecture-3/src/access-modifier.ts
+++ Lecture-3/src/access-modifier.ts
@@ -1,35 +1,35 @@
-//"Private" modifier used when we can't change any properties value.
-// "Protected" modifier used to access any class properties on another class properties. 
+// //"Private" modifier used when we can't change any properties value.
+// // "Protected" modifier used to access any class properties on another class properties. 
 
-class BankAccount {
-    id: number;
-    name: string;
-    // private _balance: number;
-    protected _balance: number;
+// class BankAccount {
+//     id: number;
+//     name: string;
+//     // private _balance: number;
+//     protected _balance: number;
 
-    constructor(id: number, name: string, balance: number) {
-        this.id = id;
-        this.name = name;
-        this._balance = balance;
-    }
+//     constructor(id: number, name: string, balance: number) {
+//         this.id = id;
+//         this.name = name;
+//         this._balance = balance;
+//     }
 
-    getBalance() {
-        console.log(`My account balance is ${this._balance}`)
-    }
+//     getBalance() {
+//         console.log(`My account balance is ${this._balance}`)
+//     }
     
-    addDeposit(amount: number) {
-        this._balance = this._balance + amount
-    }
-}
+//     addDeposit(amount: number) {
+//         this._balance = this._balance + amount
+//     }
+// }
 
-// Protected modifier
-class StudentAccount extends BankAccount {
-    test() {
-        this._balance 
-    }
-}
+// // Protected modifier
+// class StudentAccount extends BankAccount {
+//     test() {
+//         this._balance 
+//     }
+// }
 
-const myAccount = new BankAccount(444, "Persian", 40)
-console.log(myAccount)
+// const myAccount = new BankAccount(444, "Persian", 40)
+// console.log(myAccount)
 
 
---
+++ Lecture-3/src/getters-setters.ts
@@ -0,0 +1,31 @@
+class BankAccount {
+    id: number;
+    name: string;
+    // private _balance: number;
+    private _balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+        this.id = id;
+        this.name = name;
+        this._balance = balance;
+    }
+
+    // Getter
+    get balance(): number { 
+        return this._balance;
+    }
+
+   //setter
+    
+    set deposit(amount: number) { 
+        this._balance = this.balance + amount;
+    }
+    
+}
+
+const myAccount = new BankAccount(444, "Persian", 40)
+console.log(myAccount.balance)
+myAccount.deposit = 50
+
+console.log(myAccount.balance)
+
