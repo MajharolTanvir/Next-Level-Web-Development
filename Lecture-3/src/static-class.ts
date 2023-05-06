b60dfed4de1561ca47f7c61f6517e77451cb3f96
unknown <mdtanvir6070@gmail.com>
2023-05-06 19:37:47 +0600

    Polymorphism

--- Lecture-3/src/static-class.ts
+++ Lecture-3/src/static-class.ts
@@ -1,17 +1,17 @@
-//The class make static and then use/access it directly. 
+// //The class make static and then use/access it directly. 
 
-class Counter {
-    static counter: number = 0;
+// class Counter {
+//     static counter: number = 0;
 
-    static increment() {
-    return Counter.counter=Counter.counter + 1
-    }
+//     static increment() {
+//     return Counter.counter=Counter.counter + 1
+//     }
 
-    static decrement() { 
-        return Counter.counter=Counter.counter - 1
-    }
-}
+//     static decrement() { 
+//         return Counter.counter=Counter.counter - 1
+//     }
+// }
 
-console.log(Counter.increment()) 
-console.log(Counter.increment()) 
-console.log(Counter.decrement()) 
+// console.log(Counter.increment()) 
+// console.log(Counter.increment()) 
+// console.log(Counter.decrement()) 
